5edecdc97495f311cf76409b77f133ad9fc0695b
Sandro Covo <sandro@covo.ch>
2020-04-12 16:54:57 +0200

    Add empty view for form

--- juso/forms/views.py
+++ juso/forms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def form_view(request, form):
+    # TODO: If post: process form; else: render form
+
+    return render(request, 'forms/form_detail.html', {
+
+    })

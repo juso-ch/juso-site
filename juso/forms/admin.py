367a8edcc069e6e687c3744faf9f645eec2ee47f
Sandro Covo <sandro@covo.ch>
2020-04-12 17:08:40 +0200

    Add database-structure

--- juso/forms/admin.py
+++ juso/forms/admin.py
@@ -1,3 +1,107 @@
 from django.contrib import admin
 
+from content_editor.admin import ContentEditor, ContentEditorInline
+from django.utils.translation import gettext as _
 # Register your models here.
+from feincms3 import plugins
+
+from juso.forms.models import Form, FormField, RichText
+from juso.utils import CopyContentMixin
+
+
+class FormFieldInline(ContentEditorInline):
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('name', 'slug', 'input_type', 'required'),
+                'ordering',
+                'region'
+            )
+        }),
+        (_("advanced"), {
+            'classes': ('collapse',),
+            'fields': (
+                'chesoices',
+                'initial',
+                'help_text',
+            )
+        })
+    )
+
+
+@admin.register(Form)
+class FormAdmin(ContentEditor, CopyContentMixin):
+    list_display = [
+        'title',
+        'slug',
+        'created_date',
+        'section',
+        'language_code',
+    ]
+
+    list_filter = [
+        'section',
+        'language_code',
+    ]
+
+    autocomplete_fields = [
+        'section'
+    ]
+
+    search_fields = [
+        'title',
+    ]
+
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+    readonly_fields = (
+        'created_date',
+        'edited_date',
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'slug',
+                'section',
+                'created_date',
+                'edited_date',
+            )
+        }),
+        (_("settings"), {
+            'classes': ('tabbed', ),
+            'fields': (
+                'submit',
+                'success_message',
+                'success_redirect',
+            )
+        }),
+        (_('meta'), {
+            'classes': ('tabbed', ),
+            'fields': (
+                'meta_title',
+                'meta_author',
+                'meta_description',
+                'meta_image',
+                'meta_image_ppoi',
+                'meta_robots',
+                'meta_canonical',
+            )
+        }),
+        (_("translations"), {
+            'classes': ('tabbed', ),
+            'fields': (
+                'translations',
+            )
+        })
+    )
+
+    inlines = [
+        FormFieldInline.create(FormField),
+        plugins.richtext.RichTextInline.create(RichText),
+    ]
+
+    plugins = [FormField, RichText]
--- juso/forms/models.py
+++ juso/forms/models.py
@@ -1,3 +1,83 @@
-from django.db import models
+from content_editor.models import create_plugin_base
+
+from django.db import models, transaction
+from django.utils.translation import gettext_lazy as _
+
+from feincms3 import plugins
+from feincms3.mixins import TemplateMixin
 
 # Create your models here.
+from juso.sections.models import get_template_list, ContentMixin
+from juso.forms.forms import get_form_instance
+
+INPUT_TYPES = (
+    ('text', _("text")),
+    ('email', _("email")),
+    ('boolean', _("boolean")),
+    ('date', _("date")),
+    ('datetime', _("datetime")),
+    ('time', _("time")),
+    ('decimal', _("decimal")),
+    ('file', _("file")),
+    ('image', _("image")),
+    ('int', _("integer")),
+    ('choice', _("choice")),
+    ('multi', _("multiple choice")),
+    ('url', _("url")),
+    ('hidden', _("hidden")),
+)
+
+
+class Form(ContentMixin):
+    TEMPLATES = get_template_list('form', (
+        (
+            'default', ('fields', 'handlers'),
+        ),
+    ))
+
+    submit = models.CharField(max_length=200)
+    success_message = models.TextField(_("success message"), blank=True)
+    success_redirect = models.URLField(_("success redirect"), blank=True)
+
+    def get_instance(self, request):
+        return get_form_instance(self, request)
+
+    class Meta:
+        verbose_name = _("form")
+        verbose_name_plural = _("forms")
+        ordering = ['title']
+
+
+PluginBase = create_plugin_base(Form)
+
+
+class RichText(plugins.richtext.RichText, PluginBase):
+    class Meta:
+        verbose_name = _("rich text")
+        verbose_name = _("rich text")
+
+
+class FormField(PluginBase):
+    name = models.CharField(_("name"), max_length=140)
+    input_type = models.CharField(
+        _("type"), choices=INPUT_TYPES,
+        max_length=140
+    )
+    slug = models.SlugField()
+    required = models.BooleanField(_("required"))
+    help_text = models.CharField(_("help text"), max_length=240, blank=True)
+    choices = models.TextField(_("choices"), blank=True)
+    initial = models.TextField(_("initial"), max_length=240, blank=True)
+
+
+class FormEntry(models.Model):
+    form = models.ForeignKey(Form, models.CASCADE)
+
+    created = models.DateTimeField(_("created"), auto_now_add=True)
+    ip = models.GenericIPAddressField(_("ip address"), blank=True, null=True)
+
+
+class FormEntryValue(models.Model):
+    form_entry = models.ForeignKey(FormEntry, models.CASCADE)
+    field = models.ForeignKey(FormField, models.CASCADE)
+    value = models.TextField(_("value"), blank=True)

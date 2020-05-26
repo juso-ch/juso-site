4281850818988bcf1038393d7217d57ee8e1ab4a
Sandro Covo <sandro@covo.ch>
2020-05-26 15:49:22 +0200

    Add refs to other languages

--- juso/pages/templatetags/translations.py
+++ juso/pages/templatetags/translations.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.simple_tag
 def translations(page, obj=None):
-    for language,_ in settings.LANGUAGES:
+    for language, _ in settings.LANGUAGES:
         if language != page.language_code:
             if obj:
                 translation = obj.get_translation_for(language)
@@ -16,3 +16,26 @@ def translations(page, obj=None):
             translation = page.get_translation_for(language)
             if translation:
                 yield (language, translation)
+
+
+@register.simple_tag
+def translation_head(page, obj=None, scheme='https:'):
+    if obj:
+        yield obj.language_code, scheme + '//' +\
+                page.site.host + obj.get_absolute_url()
+        for translation in obj.tranlsations.all():
+            uri = translation.get_absolute_url()
+            if uri.startswith('//'):
+                uri = scheme + uri
+            else:
+                uri = scheme + '//' + page.site.host + uri
+            yield translation.language_code, uri
+    else:
+        yield page.language_code, scheme + '//' + page.site.host + page.path
+        for translation in page.translations.all():
+            uri = translation.get_absolute_url()
+            if uri.startswith('//'):
+                uri = scheme + uri
+            else:
+                uri = scheme + '//' + page.site.host + uri
+            yield translation.language_code, uri

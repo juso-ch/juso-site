fa9f768929c1ca6a5c87b0f9fcd195d96ba904fb
Sandro Covo <sandro@covo.ch>
2020-08-08 17:58:45 +0200

    Add media app if debug

--- juso/urls.py
+++ juso/urls.py
@@ -63,3 +63,9 @@ urlpatterns = [
 handler404 = "juso.pages.views.error404"
 
 handler500 = "juso.pages.views.error500"
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )

0932579e18bef73edc2ab6e5ae3cd68e64115430
Sandro Covo <sandro@covo.ch>
2020-08-18 23:09:14 +0200

    Add view to create new users

--- juso/admin.py
+++ juso/admin.py
@@ -1,8 +1,27 @@
+import secrets
+from django.contrib import messages
 from content_editor.admin import ContentEditorInline
 from django.contrib import admin
+from django.urls import path
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
+
+from django import forms
+
+from juso.sections.models import Section
+
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    section = forms.ModelChoiceField(Section.objects.all())
+    group = forms.ModelChoiceField(Group.objects.all(), required=False)
 
 
 class CustomUserAdmin(UserAdmin):
@@ -12,6 +31,86 @@ class CustomUserAdmin(UserAdmin):
             form.base_fields["email"].required = True
 
         return form
+
+    def register_new_user(self, request):
+        form = RegistrationForm(request.POST)
+
+        if request.POST:
+            if form.is_valid():
+                first_name = form.cleaned_data['first_name'].replace(' ', '')
+                last_name = form.cleaned_data['last_name'].replace(' ', '')
+                username = f'{first_name.lower()}.{last_name.lower()}'
+
+                password = secrets.token_urlsafe(16)
+
+                while User.objects.filter(username=username).exists():
+                    username = username + secrets.token_urlsafe(1)
+
+                user = User.objects.create_user(
+                    username, email=form.cleaned_data['email'], password=password,
+                    is_active=True, is_staff=True
+                )
+
+                if form.cleaned_data['group']:
+                    user.groups.add(form.cleaned_data['group'])
+
+                section = form.cleaned_data['section']
+                section.users.add(user)
+                section.save()
+                user.save()
+                message = """Hallo/Bonjour {first_name} {last_name},
+
+Dein Zugang für/Ton access pour [{section.name}]: {username} / {password}
+
+Admin: https://{section.site.host}/admin/
+
+---
+{section.name}"""
+
+                send_mail(
+                    f"Account [{section.name}]",
+                    message.format(
+                        username=username,
+                        first_name=first_name,
+                        last_name=last_name,
+                        password=password,
+                        section=section,
+                    ),
+                    'website@juso.ch',
+                    [user.email],
+                )
+
+                messages.success(
+                    request, _(f"Account for {username} created!")
+                )
+
+                return redirect(
+                    "admin:auth_user_changelist"
+                )
+
+
+
+        context = dict(
+            self.admin_site.each_context(request),
+            form=form,
+            title=_("Register new user"),
+            app_label="auth",
+            opts=User._meta,
+            model=User,
+        )
+        return render(request, 'admin/register_new_user.html', context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        return [
+            path(
+                "register-new-user/",
+                user_passes_test(lambda user: user.is_superuser, login_url='/admin/login/')(
+                    self.admin_site.admin_view(self.register_new_user)
+                ),
+                name="auth_User_register_new_user",
+            )
+        ] + urls
 
 
 class ButtonInline(ContentEditorInline):
